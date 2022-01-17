b70af1a5b4eb37416ba7623a5a73ebf7f80835ad
Ravi Kiran <kiran@crayond.co>
2022-01-18 03:34:08 +0530

    private route added to casbin

--- src/router/privateRouter.js
+++ src/router/privateRouter.js
@@ -1,50 +1,62 @@
 /**
  * @author AUTHOR_NAME
  * @email AUTHOR_EMAIL
- * @create date 
- * @modify date 
+ * @create date
+ * @modify date
  * @desc The private route check wheather the user is logged in or not and also check
  * wheather the request route from the user is accessible to them or not using the Access(role,path)
  * function then allow to the particular route or else it will redirect to login page.
  */
 
-import React from "react";
-import { Redirect, Route } from "react-router-dom";
-import { Routes } from "./routes";
-import { Access } from "./access";
-import AppDrawer from "../App.drawer";
-
-const PrivateRoute = ({ children, ...rest }) => {
-  
-  const isAuthenticated = (router) => {   
-    if (1) {
-      const _ = Access(1, router?.match?.path);
-      if (_ >= 0) {
-        return true;
-      };
-      return false;
-    } else {
-      return false;
-    }
-  };
+ import React from "react";
+ import { Redirect, Route } from "react-router-dom";
+ import { Routes } from "./routes";
+ import { useDispatch } from "react-redux";
  
-  return (
-    <Route
-      {...rest}
-      render={(_) =>
-        isAuthenticated(_) ? (
-          <AppDrawer>{children}</AppDrawer>
+ const PrivateRoute = (routeprops) => {
+ 
+   const dispatch = useDispatch();
+   
+   let { component: Component, path, isIdmEnabled, ...rest } = routeprops;
+ 
+   const checkAccess = () => {
+     try {
+       if(isIdmEnabled){
+        let data = localStorage.getItem('role_data');
+        const accessObj = JSON.parse(atob(data))?.[localStorage.getItem('role_name')]?.access || [];
+        if (accessObj?.length === 0) {
+          throw new Error("Invalid Permissions")
+        }
+        return localStorage.getItem('role_name') && accessObj.indexOf(path) > -1 && sessionStorage.token;
+       }else{
+        return localStorage.getItem('auth_token')
+       }
+     } catch (err) {
+       sessionStorage.removeItem("token")
+       sessionStorage.removeItem("role_name")
+       sessionStorage.removeItem("role_data")
+       sessionStorage.removeItem("remove_item")
+       return false;
+     }
+   };
+ 
+   return (
+      <Route
+        {...rest} render={(props) => checkAccess() ? (
+          <Component {...props} />
         ) : (
-            <Redirect
-              to={{
-                pathname: Routes.login,
-                state: { from: _?.location },
-              }}
-            />
-          )
-      }
-    />
-  );
-};
-
-export default PrivateRoute;
+          <Redirect to={"/"} />
+        )
+        }
+      />
+      // <Route
+      //   {...rest} render={(props) =>  (
+      //     <Component {...props} />
+      //   ) 
+      //   }
+      // />
+    );
+ };
+ 
+ export default PrivateRoute;
+ 
